93a43a8beb2f0928f228ba8c0b98746d98d2e840
piyush-2511 <piyushkumar76349856@gmail.com>
2024-12-29 12:25:12 +0530

    creating the home route

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cookieParser = require('cookie-parser');
 const path = require('path');
 const dotenv = require('dotenv');
 const {connectToDb} = require('./config/db.js');
+const userRoutes = require('./routes/user.routes.js');
+const indexRoutes = require('./routes/home.routes.js')
 
 dotenv.config(); // Load environment variables
 
@@ -21,9 +23,14 @@ app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
 app.set('view engine', 'ejs');
 
 // Routes (You need to import and use your user routes here)
-const userRoutes = require('./routes/user.routes.js');
 app.use('/user', userRoutes); // Use the user routes under '/user'
+app.use('/',indexRoutes)
 
+// Global error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 // Start the server
 (async () => {
   await connectToDb(); // Ensure connection is established
