c9fb473aa349039b9cdc15519345bf20fbccbb84
Piyush <150579220+piyush-2511@users.noreply.github.com>
2024-12-29 18:38:39 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { validationResult, body } = require('express-validator');
 const userModel = require('../models/user.models.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 
